cec68dff5b057bf60d77483e72452a675c5630b4
Samyra Torres <samyratorres476@gmail.com>
2025-11-19 23:12:01 -0300

    Atividade do pula-pula refeita

--- myrep/poo/pula-pula/py/draft.py
+++ myrep/poo/pula-pula/py/draft.py
@@ -1,83 +1,74 @@
-class Criança: 
-    def __init__ (self, nome: str, idade: int):
-        self.__idade = idade
-        self.__nome = nome 
+class Criança:
+    def __init__ (self, name: str, age: int):
+        self.__name = name 
+        self.__age = age
 
     def __str__ (self):
-        return f"{self.__nome}:{self.__idade}"
+        return f"{self.__name}:{self.__age}"
     
     def get_name (self):
-        return self.__nome
-    
-    def get_age (self):
-        return self.__idade
-    
-    def set_name (self, name: str):
-        self.__nome = name
-        return self.__nome
-    
-    def set_age (self, age: str):
-        self.__idade = age
-        return self.__idade
+        return self.__name
     
 class Trampolim:
     def __init__ (self):
-        self.brincando: list [Criança | None] = []
-        self.espera: list [Criança | None] = []
+        self.brincando: list[Criança] = []
+        self.esperando: list[Criança] = []
     
-    def __str__ (self):
-        espera = ", ".join([str(x) for x in self.espera])
-        brincando = ", ".join([str(x) for x in self.brincando])
+    def __str__(self):
+        espera = ", ".join([str(i) for i in self.esperando])
+        brincando = ", ".join([str(i) for i in self.brincando])
         return f"[{espera}] => [{brincando}]"
     
     def arrive (self, criança: Criança):
-        self.espera.insert(0, criança)
+        self.esperando.insert(0, criança)
 
     def enter (self):
-        criança = self.espera.pop()
+        criança = self.esperando.pop()
         self.brincando.insert(0, criança)
         del criança
-    
+
     def leave (self):
         if len(self.brincando) == 0:
             return
         criança = self.brincando.pop()
-        self.espera.insert(0, criança)
+        self.esperando.insert(0, criança)
         del criança
 
     def remove (self, nome: str):
-        for i, criança in enumerate(self.espera):
+        for i, criança in enumerate(self.esperando):
             if criança.get_name() == nome:
-                self.espera.remove(criança)
+                self.esperando.remove(criança)
                 return
         for i, criança in enumerate(self.brincando):
             if criança.get_name() == nome:
                 self.brincando.remove(criança)
                 return
-        print(f"fail: {nome} nao esta no pula-pula")
+        print (f"fail: {nome} nao esta no pula-pula")
+        
 
 def main():
     trampolim = Trampolim()
-    while True:
+    while True: 
         line = input()
         args: list[str] = line.split(" ")
         print ("$" + line)
         if args[0] == "end":
-            break 
+            break
         elif args[0] == "arrive":
             nome = args[1]
             idade = int(args[2])
             criança = Criança(nome, idade)
             trampolim.arrive(criança)
         elif args[0] == "show":
-            print(trampolim)
+            print (trampolim)
         elif args[0] == "enter":
             trampolim.enter()
         elif args[0] == "leave":
             trampolim.leave()
         elif args[0] == "remove":
-            remover = args[1]
-            trampolim.remove(remover)
+            nome = args[1]
+            trampolim.remove(nome)
         else:
             print("fail: comando inválido")
+
 main()
